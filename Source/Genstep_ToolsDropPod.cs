6807d6d3a15faba78cdb9ce9597ab62b8b61fee8
Wivex <wivexorium@gmail.com>
2015-08-17 14:53:02 +0300

    test1
    test2

---
+++ Source/Genstep_ToolsDropPod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace ToolsForJobs
+{
+    class Genstep_ToolsDropPod : Genstep
+    {
+        public override void Generate()
+        {
+            List<Thing> listOfThingsToDrop = new List<Thing>();
+
+            ThingDef handaxeDef = DefDatabase<ThingDef>.GetNamed("TFJ_Tool_Woodcutting_Handaxe");
+            ThingDef pickaxeDef = DefDatabase<ThingDef>.GetNamed("TFJ_Tool_Mining_Pickaxe");
+            ThingDef hammerDef = DefDatabase<ThingDef>.GetNamed("TFJ_Tool_Building_Hammer");
+
+            Thing handaxe = ThingMaker.MakeThing(handaxeDef, GenStuff.DefaultStuffFor(handaxeDef));
+            Thing pickaxe = ThingMaker.MakeThing(pickaxeDef, GenStuff.DefaultStuffFor(pickaxeDef));
+            Thing hammer = ThingMaker.MakeThing(hammerDef, GenStuff.DefaultStuffFor(hammerDef));
+
+            listOfThingsToDrop.Add(handaxe);
+            listOfThingsToDrop.Add(pickaxe);
+            listOfThingsToDrop.Add(hammer);
+
+            DropPodUtility.DropThingsNear(MapGenerator.PlayerStartSpot, listOfThingsToDrop, 110, MapInitData.StartedDirectInEditor, true);
+        }
+    }
+}
---
+++ Source/WorkGiver_WorkWithTools_HunterHunt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace ToolsForJobs
+{
+    class WorkGiver_WorkWithTools_HunterHunt : WorkGiver_HunterHunt
+    {
+        public override bool ShouldSkip(Pawn pawn)
+        {
+            return !WorkGiver_HunterHunt.HasHuntingWeapon(pawn) || pawn.equipment.Primary.def.defName.Contains("TFJ_Tool") || WorkGiver_HunterHunt.HasShieldAndRangedWeapon(pawn);
+        }
+    }
+}
